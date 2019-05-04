fb7a70d0b80691de8c942b9c522359acfa051541
Melvin Idema <melvin_i13@hotmail.com>
2019-05-04 10:53:02 +0200

    Add alphabetical character support

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Segment {
 
     this.segments = [this.a, this.b, this.c, this.d, this.e, this.f, this.g, this.dp];
 
-    this.segmentNumbers = {
+    this.segmentCharacters = {
       //   a  b  c  d  e  f  g  dp
       0: [ 1, 1, 1, 1, 1, 1, 0, 0],
       1: [ 0, 1, 1, 0, 0, 0, 0, 0],
@@ -25,13 +25,40 @@ class Segment {
       7: [ 1, 1, 1, 0, 0, 0, 0, 0],
       8: [ 1, 1, 1, 1, 1, 1, 1, 0],
       9: [ 1, 1, 1, 1, 0, 1, 1, 0],
-      dp: [ 0, 0, 0, 0, 0, 0, 0, 1]
+      dp:[ 0, 0, 0, 0, 0, 0, 0, 1],
+      a: [ 1, 1, 1, 0, 1, 1, 1, 0],
+      b: [ 0, 0, 1, 1, 1, 1, 1, 0],
+      c: [ 1, 0, 0, 1, 1, 1, 0, 0],
+      d: [ 0, 1, 1, 1, 1, 0, 1, 0],
+      e: [ 1, 0, 0, 1, 1, 1, 1, 0],
+      f: [ 1, 0, 0, 0, 1, 1, 1, 0],
+      h: [ 0, 1, 1, 0, 1, 1, 1, 0],
+      i: [ 0, 0, 0, 0, 1, 1, 0, 0],
+      j: [ 0, 1, 1, 1, 0, 0, 0, 0],
+      l: [ 0, 0, 0, 1, 1, 1, 0, 0],
+      n: [ 0, 0, 1, 0, 1, 0, 1, 0],
+      o: [ 0, 0, 1, 1, 1, 0, 1, 0],
+      p: [ 1, 1, 0, 0, 1, 1, 1, 0],
+      q: [ 1, 1, 1, 0, 0, 1, 1, 0],
+      r: [ 0, 0, 0, 0, 1, 0, 1, 0],
+      s: [ 1, 0, 1, 1, 0, 1, 1, 0],
+      u: [ 0, 1, 1, 1, 1, 1, 0, 0],
+      y: [ 0, 1, 1, 1, 0, 1, 1, 0],
+      z: [ 1, 1, 0, 1, 1, 0, 1, 0]
     };
 
   }
 
-  write(number) {
-    this.segmentNumbers[number].forEach((number, index) => {
+  write(character) {
+    if(typeof character === "string") {
+      character = character.toLowerCase();
+    }
+
+    if(!this.segmentCharacters.hasOwnProperty(character)) {
+      throw new Error(`The character ${character} is not supported.`);
+    }
+
+    this.segmentCharacters[character].forEach((number, index) => {
       this.segments[index].writeSync(number);
     });
   };
